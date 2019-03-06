f8c69cad7282ebcd6d29717a72ccca1c4fa0a134
DzLadus <46032365+DzLadus@users.noreply.github.com>
2019-03-06 15:50:36 +0300

    Create 2-1.cpp

---
+++ 2-1.cpp
@@ -0,0 +1,90 @@
+//В массиве А(N,N) найти первую строку, не содержащую отрицательных элементов, и поменять её с последней строкой.
+//Из "Полечгче"
+#include <iostream>
+#include <time.h>
+#include <random>
+#include <iomanip>
+
+using namespace std;
+
+int** give_memory(int, int);
+void init_array(int **, int, int);
+void mainTask(int **, int, int);
+void print_array(int **, int, int);
+void free_array(int **, int);
+
+
+int main() {
+	int N, M;
+	cout << "Enter count of columns and count of rows: ";
+	cin >> N >> M;
+	int** p = give_memory(N, M);
+	cout << "Main address " << p << '\n';
+	for (int i = 0; i < N; i++) {
+		cout << p[i] << " address " << i + 1 << '\n';
+	}
+	init_array(p, N, M);
+	print_array(p, N, M);
+	mainTask(p, N, M);
+	print_array(p, N, M);
+	free_array(p, N);
+	system("pause");
+	return 0;
+}
+
+int** give_memory(int N, int M) {
+	int** p = new(nothrow) int* [N];
+	for (int i = 0; i < N; ++i) {
+		int* p1 = new(nothrow) int[M];
+		p[i] = p1;
+	}
+	return p;
+}
+
+void init_array(int** p, int N, int M) {
+	srand(time(NULL));
+	for (int i = 0; i < N; i++) {
+		int* p1 = p[i];
+		for (int g = 0; g < M; g++) {
+			p1[g] = -10 + (rand() % 100);
+		}
+	}
+}
+
+void print_array(int** p, int N, int M) {
+	for (int i = 0; i < N; i++) {
+		int* p1 = p[i];
+		for (int g = 0; g < M; g++) {
+			cout << setw(5) << p1[g];
+		}
+		cout << '\n';
+	}
+	cout << '\n';
+}
+
+void free_array(int** p, int N) {
+	for (int i = 0; i < N; i++) {
+		p[i] = nullptr;
+	}
+	delete[]p;
+}
+
+void mainTask(int** p, int N, int M) {
+	bool flag;
+	for (int i = 0; i < N; i++) {
+		int* p1 = p[i];
+		int g;
+		for (flag = true, g = 0; g < M; g++) {
+			if (p1[g] < 0) {
+				flag = false;
+				break;
+			}
+		}
+		if (flag) {
+			int* temp = p[i];
+			p[i] = p[N - 1];
+			p[N - 1] = temp;
+			break;
+		}
+	}
+}
