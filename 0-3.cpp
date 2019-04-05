b293943313d003e952eef8d7283a3adfe872fb0c
DzLadus <46032365+DzLadus@users.noreply.github.com>
2019-04-05 13:22:21 +0300

    Create 0-3.cpp

---
+++ 0-3.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <random>
+#include <time.h>
+#include <iomanip>
+
+using namespace std;
+
+int* give_memory(int N);
+void init_array(int* p, int N);
+void shifting(int* p, int N, int k);
+void print_array(int* p, int N);
+
+int main() {
+
+	cout << "Enter number of elements: ";
+	int N; cin >> N;
+	cout << "Enter number K: ";
+	int k; cin >> k;
+
+	int* p = give_memory(N);
+	if (!p) { cout << "No memory!"; }
+
+	init_array(p, N);
+	print_array(p, N);
+	shifting(p, N, k);
+	print_array(p, N);
+
+	delete[]p;
+	p = nullptr;
+	system("pause");
+	return 0;
+}
+
+int* give_memory(int N) {
+	int* p = new(nothrow) int[N];
+	if (p == nullptr) { return nullptr; }
+	return p;
+}
+
+void init_array(int* p, int N){
+	srand(time(NULL));
+	int a = 10;
+	for (int i = 0; i < N; i++) {
+		p[i] = rand() % a;
+	}
+}
+
+void print_array(int* p, int N) {
+	for (int i = 0; i < N; i++) {
+		cout << setw(5) << p[i];
+	}
+	cout << '\n';
+}
+
+void shifting(int* p, int N, int k) {
+	int tmp = 0;
+	for (int i = 0; i < k; i++) {
+		tmp = p[N - 1];
+		for (int g = N - 2; g >= 0; g--) { p[g + 1] = p[g]; }
+		p[0] = tmp;
+	}
+}
