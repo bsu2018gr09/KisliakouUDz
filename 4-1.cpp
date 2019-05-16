e45989c79b4a58a37c2a658551254cd78ac5b0b5
DzLadus <46032365+DzLadus@users.noreply.github.com>
2019-05-16 10:33:19 +0300

    Update 4-1.cpp

--- 4-1.cpp
+++ 4-1.cpp
@@ -1,15 +1,16 @@
 //Отсортировать строки по количеству слов заданной длины
+#define _CRT_SECURE_NO_WARNINGS
 
 #include<iostream>
 #include<fstream> 	
+#include<cstring>
 
 using namespace std;
 
 char* getNextWordStart(char* pointer);
-void printWordinFile(char* pointer); 
-void printWordOnScreen(char* pointer);
 int lettersInWord(char* pointer);
-char* mainTask(char* start, int count);
+int mainTask(char* start, int count);
+void sort(char**, int*, int);
 
 ifstream input("d:\\WarAndPiece.txt");
 ofstream output("d:\\result.txt");
@@ -18,24 +19,58 @@ int main() {
 	cout << "Enter count of letters in words: ";
 	int count; cin >> count;
 
-	const int N = 400;
-	char *start = new char[N];
+	const int N = 300;
+	int rowsCount = 0;
 
-	if (!input) cout << "No file d:\\date1.txt. Can't open\n"; 
-	if (!output) cout << "File d:\\date2.txt. Can't create\n";
+	char* start = new char[N];
+	char** rows = new char*[N];
+	int *wordsInRowCount = new(nothrow) int[N];
 
-	char* tmp = start;
+	if (!input) cout << "No file d:\\WarAndPiece.txt. Can't open\n";
+	if (!output) cout << "File d:\\result.txt. Can't create\n";
+
+	
 	while (true) {
-		input.getline(start, N - 1);
-		tmp = start;
-		while(tmp)
-			tmp = mainTask(tmp, count);
-		if (input.eof()) 
-			break;
+		input.getline(start, N + 1);
+		rows[rowsCount] = new(nothrow) char[strlen(start) + 1];
+		strcpy(rows[rowsCount], start);
+		wordsInRowCount[rowsCount] = mainTask(rows[rowsCount], count);
+		rowsCount++;
+		if (input.eof()) { break; }
 	}
-	cout << '\n';
-	delete[]start;
-	delete[]tmp;
+	
+	bool flag = true;
+	while (flag) {
+		for (int g = 0; g < rowsCount - 1; g++) {
+			if (wordsInRowCount[g] > wordsInRowCount[g + 1]) {
+				int tmp = wordsInRowCount[g];
+				char* tmp1 = rows[g];
+
+				wordsInRowCount[g] = wordsInRowCount[g + 1];
+				wordsInRowCount[g + 1] = tmp;
+
+				rows[g] = rows[g + 1];
+				rows[g + 1] = tmp1;
+			}
+		}
+		if (flag) { break; }
+		else { flag = true; }
+	}
+
+	for (int g = 0; g < rowsCount; g++) {
+		output << wordsInRowCount[g] << ' ' << rows[g] << '\n';
+	}
+	
+	
+	for (int g = 0; g < rowsCount; g++) {
+		cout << rows[g] << " ";
+		cout << wordsInRowCount[g] << '\n';
+	}
+	
+	
+	delete[]rows;
+	delete[]wordsInRowCount;
+	
 	input.close();
 	output.close();
 
@@ -46,49 +81,33 @@ int main() {
 char* getNextWordStart(char* pointer) {
 	int i = 0;
 	while (pointer[i] != ' ') {
-		if (pointer[i] == 0)
-			return 0;
+		if (!pointer[i])
+			return nullptr;
 		i++;
 	}
 	while (pointer[i] == ' ') {
-		if (pointer[i] == 0)
-			return 0;
+		if (!pointer[i + 1])
+			return nullptr;
 		i++;
 	}
 	return (pointer + i);
 }
 
-void printWordinFile(char* pointer) {
-	int i = 0;
-	while (pointer[i] >= 65) {
-		output << pointer[i];
-		i++;
-	}
-}
-
-void printWordOnScreen(char* pointer) {
-	int i = 0;
-	while (pointer[i] >= 65) {
-		cout << pointer[i];
-		i++;
-	}
-}
-
 int lettersInWord(char* pointer) {
 	int i = 0, counter = 0;
-	while (pointer[i] >= 65) {
+	while ((pointer[i] >= 65 && pointer[i] <= 90) || (pointer[i] >= 97 && pointer[i] <= 122)) {
 		counter++;
 		i++;
 	}
 	return counter;
 }
 
-char* mainTask(char* start, int count) {
-	if (lettersInWord(start) == count) {
-		printWordOnScreen(start);
-		cout << ' ';
-		printWordinFile(start);
-		output << ' ';
-	} 
-	return getNextWordStart(start);
+int mainTask(char* start, int count) {
+	int counter = 0;
+	while(start){
+		if (lettersInWord(start) == count) { counter++; }
+		start = getNextWordStart(start);
+	}
+	return counter;
 }
+
